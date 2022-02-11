2ab2328c810246783047284eabff1de44d1055dc
Mouhamadou98 <modzigning@gmail.com>
2022-02-11 19:50:34 +0000

    fizzbuzz app in java with unit test

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -6,10 +6,10 @@ public class FizzBuzz {
 		
 		if(number %15 == 0)
 			return "FizzBuzz";
+		if(number %5 == 0)
+			return "Buzz";
 		if(number %3 == 0)
 			return "Fizz";
-		if(number % 5 == 0)
-			return "Buzz";
 		
 		return "" + number;
 	}
